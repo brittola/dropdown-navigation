d7a82ef3957ec0921e0234300a9532730ebd62c9
Gabriel Rodrigues <99913525+brittola@users.noreply.github.com>
2022-06-01 15:32:07 -0300

    changing script to JQuery
    i've been studying JQuery in the last days so i decided to use it on this project for practicing

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 let jQ = $.noConflict();
 
-let features = document.getElementById('features');
+let features = jQ('#features');
 let featuresList = jQ("#featuresList");
-let company = document.getElementById('company');
+let company = jQ('#company');
 let companyList = jQ("#companyList");
-let featuresArrow = document.getElementById('featuresArrow');
-let companyArrow = document.getElementById('companyArrow');
+let featuresArrow = jQ('#featuresArrow');
+let companyArrow = jQ('#companyArrow');
 
 function showFeatures() {
     if(featuresList.css('display') == '' || featuresList.css('display') == 'none'){
         featuresList.slideDown(300);
-        featuresArrow.setAttribute('src', './assets/icon-arrow-up.svg');
+        featuresArrow.attr('src', './assets/icon-arrow-up.svg');
         companyList.slideUp(1);
-        companyArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+        companyArrow.attr('src', './assets/icon-arrow-down.svg');
     }else{
         featuresList.slideUp(300);
-        featuresArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+        featuresArrow.attr('src', './assets/icon-arrow-down.svg');
     }
 }
 
 function showCompany() {
     if(companyList.css('display') == '' || companyList.css('display') == 'none'){
         companyList.slideDown(300);
-        companyArrow.setAttribute('src', './assets/icon-arrow-up.svg');
+        companyArrow.attr('src', './assets/icon-arrow-up.svg');
         featuresList.slideUp(1);
-        featuresArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+        featuresArrow.attr('src', './assets/icon-arrow-down.svg');
     }else{
         companyList.slideUp(300);
-        companyArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+        companyArrow.attr('src', './assets/icon-arrow-down.svg');
     }
 }
 
 function hideMenus(){
     featuresList.slideUp(1);
-    featuresArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+    featuresArrow.attr('src', './assets/icon-arrow-down.svg');
     companyList.slideUp(1);
-    companyArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+    companyArrow.attr('src', './assets/icon-arrow-down.svg');
 }
 
-features.addEventListener('click', showFeatures);
-company.addEventListener('click', showCompany);
+features.click(showFeatures);
+company.click(showCompany);
+
 document.body.addEventListener('click', (event) => {
     if(event.target.id != 'features' && event.target.id != 'company'){
         hideMenus();
@@ -49,14 +50,14 @@ document.body.addEventListener('click', (event) => {
 //-------------------------- MOBILE VERSION ------------------------------------
 
 let menuScreen = jQ('.menu-screen');
-let mobileFeatures = document.getElementById('mobileFeatures');
-let mobileFeaturesList = document.getElementById('mobileFeaturesList');
-let mobileCompany = document.getElementById('mobileCompany');
-let mobileCompanyList = document.getElementById('mobileCompanyList');
-let mobileFeaturesArrow = document.getElementById('mobileFeaturesArrow');
-let mobileCompanyArrow = document.getElementById('mobileCompanyArrow');
-let openMenu = document.getElementById('openMenu');
-let closeMenu = document.getElementById('closeMenu');
+let mobileFeatures = jQ('#mobileFeatures');
+let mobileFeaturesList = jQ('#mobileFeaturesList');
+let mobileCompany = jQ('#mobileCompany');
+let mobileCompanyList = jQ("#mobileCompanyList");
+let mobileFeaturesArrow = jQ('#mobileFeaturesArrow');
+let mobileCompanyArrow = jQ('#mobileCompanyArrow');
+let openMenu = jQ('#openMenu');
+let closeMenu = jQ('#closeMenu');
 
 function showMenu(){
     menuScreen.css('display', 'flex').fadeOut(0).fadeIn(200);
@@ -67,26 +68,26 @@ function hideMenu(){
 }
 
 function showMobileFeatures(){
-    if(mobileFeaturesList.style.display == '' || mobileFeaturesList.style.display == 'none'){
-        mobileFeaturesList.style.display = 'flex';
-        mobileFeaturesArrow.setAttribute('src', './assets/icon-arrow-up.svg');
+    if(mobileFeaturesList.css('display') == '' || mobileFeaturesList.css('display') == 'none'){
+        mobileFeaturesList.slideDown(300);
+        mobileFeaturesArrow.attr('src', './assets/icon-arrow-up.svg');
     }else{
-        mobileFeaturesList.style.display = 'none';
-        mobileFeaturesArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+        mobileFeaturesList.slideUp(300);
+        mobileFeaturesArrow.attr('src', './assets/icon-arrow-down.svg');
     }
 }
 
 function showMobileCompany(){
-    if(mobileCompanyList.style.display == '' || mobileCompanyList.style.display == 'none'){
-        mobileCompanyList.style.display = 'flex';
-        mobileCompanyArrow.setAttribute('src', './assets/icon-arrow-up.svg');
+    if(mobileCompanyList.css('display') == '' || mobileCompanyList.css('display') == 'none'){
+        mobileCompanyList.slideDown(300);
+        mobileCompanyArrow.attr('src', './assets/icon-arrow-up.svg');
     }else{
-        mobileCompanyList.style.display = 'none';
-        mobileCompanyArrow.setAttribute('src', './assets/icon-arrow-down.svg');
+        mobileCompanyList.slideUp(300);
+        mobileCompanyArrow.attr('src', './assets/icon-arrow-down.svg');
     }
 }
 
-openMenu.addEventListener('click', showMenu);
-closeMenu.addEventListener('click', hideMenu);
-mobileFeatures.addEventListener('click', showMobileFeatures);
-mobileCompany.addEventListener('click', showMobileCompany);
+openMenu.click(showMenu);
+closeMenu.click(hideMenu);
+mobileFeatures.click(showMobileFeatures);
+mobileCompany.click(showMobileCompany);
